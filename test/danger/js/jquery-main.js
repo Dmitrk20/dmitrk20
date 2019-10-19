7f1fd0aab6d58648de2a98a2d5032054cb870f3b
Dmitrk20 <dmitrk20@gmail.com>
2019-10-19 15:17:30 +1000

    1

--- test/danger/js/jquery-main.js
+++ test/danger/js/jquery-main.js
@@ -597,29 +597,29 @@ $(document).ready(function(){
 
 
 
-	$('.call-accept').click(function(){
-	$('.thx').addClass('thx-show');
-	$('.call').removeClass('show');
-	$('.close').click(function(){
-	$('.thx').removeClass('thx-show');
-	});
-	});
-
-	$('.credit-accept').click(function(){
-	$('.thx').addClass('thx-show');
-	$('.modal-credit').removeClass('open-credit');
-	$('.close').click(function(){
-	$('.thx').removeClass('thx-show');
-	});
-	});
-
-	$('.test-accept').click(function(){
-	$('.thx').addClass('thx-show');
-	$('.modal-testDrive').removeClass('open-test');
-	$('.close').click(function(){
-	$('.thx').removeClass('thx-show');
-	});
-	});
+	// $('.call-accept').click(function(){
+	// $('.thx').addClass('thx-show');
+	// $('.call').removeClass('show');
+	// $('.close').click(function(){
+	// $('.thx').removeClass('thx-show');
+	// });
+	// });
+	//
+	// $('.credit-accept').click(function(){
+	// $('.thx').addClass('thx-show');
+	// $('.modal-credit').removeClass('open-credit');
+	// $('.close').click(function(){
+	// $('.thx').removeClass('thx-show');
+	// });
+	// });
+	//
+	// $('.test-accept').click(function(){
+	// $('.thx').addClass('thx-show');
+	// $('.modal-testDrive').removeClass('open-test');
+	// $('.close').click(function(){
+	// $('.thx').removeClass('thx-show');
+	// });
+	// });
 
 
 
