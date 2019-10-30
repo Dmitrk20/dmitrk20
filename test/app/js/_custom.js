f7c8007ac3b7e20adcd25df6898f5b12bcd97a54
Dmitrk20 <dmitrk20@gmail.com>
2019-10-30 10:25:23 +1000

    12

--- test/app/js/_custom.js
+++ test/app/js/_custom.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
+
   $(".owl-carousel").owlCarousel({
     items : 2,
-    nav: true
+    nav: true,
+    responsive : {
+      576 : {
+        items : 3,
+      },
+      768 : {
+        items : 2,
+      }
+    }
   });
 
 
@@ -13,6 +23,56 @@ document.addEventListener("DOMContentLoaded", function() {
 
   $('.popup-closed').click(function(){
     $('.popup').css('display','none')
+  });
+
+  $('#project_1').click(function(){
+    $('.active').removeClass('active')
+    $('#project_1').addClass('active')
+    $('.project-item-prev-text span').html('Пентхаус')
+  });
+
+  $('#project_2').click(function(){
+    $('.active').removeClass('active')
+    $('#project_2').addClass('active')
+    $('.project-item-prev-text span').html('Дома')
+  });
+
+  $('#project_3').click(function(){
+    $('.active').removeClass('active')
+    $('#project_3').addClass('active')
+    $('.project-item-prev-text span').html('Бассейны')
+  });
+
+  $('#project_4').click(function(){
+    $('.active').removeClass('active')
+    $('#project_4').addClass('active')
+    $('.project-item-prev-text span').html('Коммерческие')
+  });
+
+  $('#project_5').click(function(){
+    $('.active').removeClass('active')
+    $('#project_5').addClass('active')
+    $('.project-item-prev-text span').html('Промышленные')
+  });
+
+  $('#project_1_1').click(function(){
+    $('.active-prev').removeClass('active-prev')
+    $('#project_1_1').addClass('active-prev')
+  });
+
+  $('#project_1_2').click(function(){
+    $('.active-prev').removeClass('active-prev')
+    $('#project_1_2').addClass('active-prev')
+  });
+
+  $('#project_1_3').click(function(){
+    $('.active-prev').removeClass('active-prev')
+    $('#project_1_3').addClass('active-prev')
+  });
+
+  $('#project_1_4').click(function(){
+    $('.active-prev').removeClass('active-prev')
+    $('#project_1_4').addClass('active-prev')
   });
 
 
