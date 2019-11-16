74c1dc3245e582addc92e2d6f8990f67fd09d793
Dmitrk20 <dmitrk20@gmail.com>
2019-11-16 22:39:32 +1000

    ffffffffff

---
+++ kran.skupil.ru/callback/js/callback.js
@@ -0,0 +1,23 @@
+﻿
+
+$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".cme-cls").click(function(){	// Событие клика на затемненный фон	   
+		$(".callback").fadeOut(100);	// Медленно убираем всплывающее окно
+	});
+
+});
+
+function showPopup(e) { 
+  e.preventDefault();
+  if(e.target.attributes['data-th']) {
+    if($(".callback input[name=Item]").size() > 0) {
+      $(".callback input[name=Item]").val(e.target.attributes['data-th'].value);
+    }
+    else {
+     $(".callback form").prepend('<input type="hidden" name="Форма звонка" value="'+e.target.attributes['data-th'].value+'">');
+    }
+  }
+  $(".callback").fadeIn(100);
+}
+
---
+++ kran.skupil.ru/js/main-scripts.js
@@ -0,0 +1,183 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+	nav : true,
+	pagination : true,
+	loop: true,
+    items: 1,
+    margin: 10,
+    dots: false,
+	autoplay:true,
+	autoplayTimeout:15000,
+	autoplayHoverPause:true
+  });
+});
+
+$(document).ready(function(){
+	$(".block-scroll").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top - 0}, 1000);
+	});
+});
+
+$(document).ready(function(){
+  
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+        });
+          
+        $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+  
+});
+
+/* ---------- #gall ----------- */
+$(document).ready(function () {
+  $(".gallery-img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+});
+/* ---------- #gall ----------- */
+
+/* ---------- #exitblock ----------- */
+$(document).mouseleave(function(e){
+    if (e.clientY < 100) {
+        $(".exitblock").fadeIn("fast");
+    }    
+});
+$(document).click(function(e) {
+    if (($(".exitblock").is(':visible')) && (!$(e.target).closest(".exitblock .modaltext").length)) {
+        $(".exitblock").remove();
+    }
+});
+/* ---------- #exitblock ----------- */
+
+/* ---------- #testim ----------- */
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
+
+/* ---------- #testim ----------- */
